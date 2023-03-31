af03d9278f4de1a068b1028b26e76636f77ea02d
Alex880326 <124200830+Alex880326@users.noreply.github.com>
2023-03-31 19:43:19 +0300

    Input elements

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите элементы массива, разделяя их запятой:");
+        string input = Console.ReadLine();
+
+        string[] array = input.Split(',');
+        string[] newArray = new string[array.Length];
+
+        
+    }
+}
